8785dc1f524d21c0ead2d39a1e0e9b32da60ff3d
David Segovia <dsegovialey@gmail.com>
2023-04-13 07:13:47 -0600

    Fixed .env for next

--- react-status/src/helpers/axios.js
+++ react-status/src/helpers/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { saveAllServices, saveGroupsInsights, saveServicesByGroup,saveServiceInfo, saveIncidentHistory } from "@/actions/services";
 
+const HOST = process.env.NEXT_PUBLIC_IP_CONFIG
 
 export const status = async () => {
     const res = await axios.get(process.env.IP_CONFIG);
@@ -9,7 +10,7 @@ export const status = async () => {
 }
 export const getGroups = () => {
     return async (dispatch) => {
-        const res = await axios.get('http://localhost:3001/services/groupInsights');
+        const res = await axios.get(`${HOST}/services/groupInsights`);
         dispatch(saveGroupsInsights(res.data))
     }
 }
@@ -20,28 +21,28 @@ export const listenStatus = async () => {
 
 export const getAllServices = () => {
     return async (dispatch) => {
-        const res = await axios.get('http://localhost:3001/services/')
+        const res = await axios.get(`${HOST}/services/`)
         dispatch(saveAllServices(res.data))
     }
 }
 
 export const getServicesByGroup = (group) => {
     return async (dispatch) => {
-        const res = await axios.get(`http://localhost:3001/services/group/${group}`)
+        const res = await axios.get(`${HOST}/services/group/${group}`)
         dispatch(saveServicesByGroup(res.data))
     }
 }
 
 export const getServiceInfo = (name) => {
     return async (dispatch) => {
-        const res = await axios.get(`http://localhost:3001/services/name/${name}`)
+        const res = await axios.get(`${HOST}/services/name/${name}`)
         dispatch(saveServiceInfo(res.data))
     }
 }
 
 export const getIncidentHistory = () => {
     return async (dispatch) => {
-        const res = await axios.get('http://localhost:3001/services/incidentHistory')
+        const res = await axios.get(`${HOST}/services/incidentHistory`)
         dispatch(saveIncidentHistory(res.data))
     }
 }
