9b2a1b6acbbb31a4017592e9e8d055a6e54d2790
YisusSM <xxchuchoxd@outlook.com>
2023-04-13 05:38:01 -0500

    responsive added

--- react-status/src/components/Home/Groups.jsx
+++ react-status/src/components/Home/Groups.jsx
@@ -27,35 +27,35 @@ export const Groups = () => {
         const value = getServicesGroupInsight[service];
         console.log(value, 'hey')
         return (
-            <div key={service} className="container mx-auto border-b border-neutral-100 bg-neutral-50 text-neutral-800 dark:bg-neutral-50 flex shadow-md hover:shadow-lg flex-col items-center max-md:flex-wrap">
-                <div className='flex flex-row-reverse w-full max-md:w-auto max-md:order-2'><span class="material-symbols-outlined text-2xl pr-2 pt-2 max-md:text-2x1 cursor-pointer">
+            <div key={service} className="container mx-auto border-b border-neutral-100 bg-neutral-50 text-neutral-800 dark:bg-neutral-50 flex shadow-md hover:shadow-lg flex-col items-center sm:flex-row sm:max-w-2xl">
+                <div className='flex items-center justify-end w-full sm:w-auto'><span class="material-symbols-outlined text-2xl pr-2 pt-2 sm:text-2x1 cursor-pointer">
                     notifications
                 </span></div>
-                <Link className='flex justify-center flex-1 flex-col items-center text-center w-2/5 max-md:order-first' href={`/dashboard/dashboardTable/${service}`}>
-                    <h2 className="px-6 py-4 font-medium font-sans text-5xl font-semibold leading-tight tracking-normal antialiased text-xl  ">{service}</h2>
+                <Link className='flex justify-center flex-1 flex-col items-center text-center w-2/5 sm:w-auto sm:order-2' href={`/dashboard/dashboardTable/${service}`}>
+                    <h2 className="px-6 py-4 font-medium font-sans text-5xl font-semibold leading-tight tracking-normal antialiased text-xl">{service}</h2>
                 </Link>
-                <Link className=" px-6 py-4 flex justify-center space-x-2.5 pt-10 cursor-pointer" href={`/dashboard/dashboardTable/${service}`}>
+                <Link className="px-6 py-4 flex justify-center space-x-2.5 pt-10 cursor-pointer sm:order-1" href={`/dashboard/dashboardTable/${service}`}>
                     {(value.red ? <div class="flex items-center flex-col">
-                        <div class="center relative inline-block select-none  rounded-lg bg-red-500 py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white">
+                        <div class="center relative inline-block select-none rounded-lg bg-red-500 py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white">
                             <div class="mt-px md:text-xs">{value.red}</div>
                         </div>
                         <span className='font-medium md:text-xs font-sans font-semibold text-xs'>Offline</span>
                     </div> : null)}
                     {(value.yellow ? <div class="flex items-center flex-col">
-                        <div class="center relative inline-block select-none  rounded-lg bg-amber-500 py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white">
+                        <div class="center relative inline-block select-none rounded-lg bg-amber-500 py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white">
                             <div class="mt-px md:text-xs">{value.yellow}</div>
                         </div>
                         <span className='font-medium md:text-xs font-sans font-semibold text-xs'>Issue</span>
                     </div> : null)}
                     <div class="flex items-center flex-col">
-                        <div class="center relative inline-block select-none  rounded-lg bg-green-500 py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white">
+                        <div class="center relative inline-block select-none rounded-lg bg-green-500 py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white">
                             <div class="mt-px md:text-xs">{value.green}</div>
                         </div>
                         <span className='font-medium md:text-xs font-sans font-semibold text-xs'>Online</span>
                     </div>
                 </Link>
-
             </div>
+
         )
     }) : null))
 
