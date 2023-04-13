d4f57cc87eb9f2bff4f218845812f2bd0678e6fc
YisusSM <xxchuchoxd@outlook.com>
2023-04-13 09:49:06 -0500

    grid ajustado

--- react-status/src/components/Home/Groups.jsx
+++ react-status/src/components/Home/Groups.jsx
@@ -27,14 +27,14 @@ export const Groups = () => {
         const value = getServicesGroupInsight[service];
         console.log(value, 'hey')
         return (
-            <div key={service} className="container mx-auto border-b border-neutral-100 bg-neutral-50 text-neutral-800 dark:bg-neutral-50 flex shadow-md hover:shadow-lg flex-col items-center max-md:flex-wrap">
+            <div key={service} className="container mx-auto border-b border-neutral-100 bg-neutral-50 text-neutral-800 dark:bg-neutral-50 flex shadow-md hover:shadow-lg flex-col items-center max-md:flex-wrap max-h-[217px] max-w-[280px]">
                 <div className='flex flex-row-reverse w-full max-md:w-auto max-md:order-2'><span class=" material-symbols-outlined text-2xl pr-2 pt-2 max-md:text-2x1 cursor-pointer max-[400px]:text-4x1">
                     notifications
                 </span></div>
-                <Link className='flex justify-center flex-1 flex-col items-center text-center w-2/5 max-md:order-first' href={`/dashboard/dashboardTable/${service}`}>
-                    <h2 className="px-6 py-4 font-medium font-sans text-5xl font-semibold leading-tight tracking-normal antialiased text-xl max-[400px]:text-[9px] ">{service}</h2>
+                <Link className='flex justify-center flex-1 flex-col items-center text-center w-2/5 max-md:order-first pb-2' href={`/dashboard/dashboardTable/${service}`}>
+                    <h2 className="font-medium font-sans text-5xl font-semibold leading-tight tracking-normal antialiased text-xl max-[400px]:text-[9px] ">{service}</h2>
                 </Link>
-                <Link className="px-6 py-4 flex justify-center space-x-2.5 pt-10 cursor-pointer max-[400px]:pb-1  " href={`/dashboard/dashboardTable/${service}`}>
+                <Link className=" flex justify-center space-x-2.5 pt-2 cursor-pointer max-[400px]:pb-4 pb-8 " href={`/dashboard/dashboardTable/${service}`}>
                     {(value.red ? <div class="flex items-center flex-col">
                         <div class="center relative inline-block select-none  rounded-lg bg-red-500 py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white">
                             <div class="mt-px md:text-xs max-[400px]:text-[8px]">{value.red}</div>
@@ -61,7 +61,7 @@ export const Groups = () => {
 
     return (
         <>
-            <div className='h-screen w-full grid grid-cols-2 gap-4 p-6 pt-8 bg-white max-md:grid-rows-6 max-md:text-xs max-md:grid-cols-1'>
+            <div className='h-screen w-full grid grid-cols-3 gap-4 p-6 pt-8 bg-white max-md:grid-rows-6 max-md:text-xs max-md:grid-cols-1 gap-y-8 auto-rows-min'>
                 {groups}
             </div>
         </>
